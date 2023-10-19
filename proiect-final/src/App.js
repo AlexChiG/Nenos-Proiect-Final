b9aa746e2b1f15352db0ffb5e96a41cdabaccac4
Chitiga Alexandru Gabriel <alex0chitiga@gmail.com>
2023-10-19 22:33:09 +0300

    mui test

--- proiect-final/src/App.js
+++ proiect-final/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
+import css from './App.css';
+import * as React from 'react';
+// import MiniDrawer from './Components/MiniDrawer';
 
 function App() {
   return (
     <div className="App">
       test
+      {/* <MiniDrawer> </MiniDrawer> */}
     </div>
   );
 }
